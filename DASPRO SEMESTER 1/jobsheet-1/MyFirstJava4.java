0fea0f1ae4ccf0a0e92f5439b0fb7abf7b6b01ee
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-10-26 10:08:50 +0700

    initial commit

---
+++ DASPRO SEMESTER 1/jobsheet-1/MyFirstJava4.java
@@ -0,0 +1,9 @@
+public class MyFirstJava4 {
+    public static void main(String[] args) {
+
+        System.out.println("Nama Saya Ananda");
+
+    }
+}
+
+ 
---
+++ DASPRO SEMESTER 1/jobsheet-3/ContohVariabel4.java
@@ -0,0 +1,14 @@
+public class ContohVariabel4{
+public static void main(String[] args) {
+String HobySaya = "Bermain petak umpet";
+boolean isPandai = true;
+char jenisKelamin = 'P';
+byte _umur = 18;
+double $ipk = 3.24, tinggi= 1.54;
+System.out.println(HobySaya);
+System.out.println("Apakah pandai? " + isPandai);
+System.out.println("Jenis kelamin: " + jenisKelamin);
+System.out.println("Umurku saat ini: " + _umur);
+System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+}
+}
