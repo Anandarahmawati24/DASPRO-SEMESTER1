1be900e47ea4c36c1a4c84d1f55e223fdf509109
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-10-26 20:16:47 +0700

    modifikasi kode program pada hasil program

--- DASPRO SEMESTER 1/jobsheet-9/BubbleSortExample04.java
+++ DASPRO SEMESTER 1/jobsheet-9/BubbleSortExample04.java
@@ -1,10 +1,10 @@
-public class BubbleSortExample {
+public class BubbleSortExample04 {
     public static void main(String[] args) {
         int [] intData = {34,18,87,72,32,54,43};
         int temp =0;
         for (int i=0; i<intData.length; i++){
             for (int j=1; j<intData.length-i; j++){
-                if (intData[j-1] > intData[j]) {
+                if (intData[j-1] < intData[j]) {
                     //swap element
                     temp =intData[j];
                     intData[j]=intData[j-1];
