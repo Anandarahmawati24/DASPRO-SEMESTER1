cd89d6a64af1e9fe2b91031ee9cf57c7008de620
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-10-26 22:02:53 +0700

    implementasi flowchart1 tugas mandiri pertemuan 9

---
+++ DASPRO SEMESTER 1/jobsheet-9/ImplementasiFlowchart104.java
@@ -0,0 +1,10 @@
+public class ImplementasiFlowchart104 {
+    public static void main(String[] args) {
+        int[] arrayBil = new int[60];
+
+        for (int i = 0; i <60; i++) {  
+            arrayBil[i] = i+1;
+        System.out.println(arrayBil[i] );
+        }
+    }
+}
