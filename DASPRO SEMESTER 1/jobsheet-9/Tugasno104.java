84d5d5a9f13bead48ed1b98e89c68ff28ea9ba95
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-10-26 20:59:30 +0700

    kode program soal no 1 tugas mandiri

---
+++ DASPRO SEMESTER 1/jobsheet-9/Tugasno104.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Tugasno104{
+public static void main(String[] args) {
+    Scanner input04 = new Scanner(System.in);
+    System.out.println("Input jumlah elemen array : ");
+    int jumlahElemen = input04.nextInt();
+    int [] Arrayelm = new int [jumlahElemen];
+    System.out.println("masukkan " +jumlahElemen+ " angka: ");
+    for (int i =0; i<jumlahElemen; i++){
+        Arrayelm[i] = input04.nextInt();
+    }
+        int max = Arrayelm[0];
+        int min = Arrayelm[0];
+        int total = Arrayelm[0];
+    
+        for (int i=1; i<jumlahElemen ;i++) {
+            if (Arrayelm[i]> max){
+                max = Arrayelm[i];
+            }
+            if (Arrayelm[i]<min) {
+                min= Arrayelm[i];  
+            }
+            total +=Arrayelm[i];
+    }
+    double Ratarata= total/ jumlahElemen;
+    System.out.println("Nilai tertinggi: "+max);
+    System.out.println("Nilai terendah: " +min);
+    System.out.println("Nilai rata rata: " +Ratarata);
+
+}
+}
