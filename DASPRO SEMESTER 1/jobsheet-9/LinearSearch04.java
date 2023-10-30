2a1f20c07c76c95610ed1839bcc8c1fc698c109f
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-10-30 21:34:35 +0700

    modifikasi program

--- DASPRO SEMESTER 1/jobsheet-9/LinearSearch04.java
+++ DASPRO SEMESTER 1/jobsheet-9/LinearSearch04.java
@@ -1,15 +1,34 @@
-public class LinearSearch04 {
-public static void main(String[] args) {
-    int [] arrayInt = {34,18,26,48,72,20,56,63};
-    int key = 20;
-    int hasil =0;
+import java.util.Scanner;
 
-    for (int i =0; i< arrayInt.length; i++){
-        if (arrayInt [i]== key){
-            hasil =i;
-            break;
+public class LinearSearch04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jumlahElemen = input.nextInt();
+        int[] arrayInt = new int[jumlahElemen];
+
+        System.out.println("Masukkan elemen-elemen array:");
+        for (int i = 0; i < jumlahElemen; i++) {
+            System.out.print("Elemen ke-" + (i + 1) + ": ");
+            arrayInt[i] = input.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        int key = input.nextInt();
+        int hasil = -1;
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+
+        if (hasil != -1) {
+            System.out.println("Key ditemukan dalam array pada posisi indeks ke-" + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan dalam array.");
         }
     }
-    System.out.println("Key ada dalam array pada posisi indeks ke -"+hasil);
-}
 }
