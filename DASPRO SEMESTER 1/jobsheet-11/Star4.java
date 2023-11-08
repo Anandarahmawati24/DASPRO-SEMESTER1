2d463b3c3287c093c1b516941f573d1f69d77d57
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-11-08 13:10:59 +0700

    modifikasi program

--- DASPRO SEMESTER 1/jobsheet-11/Star4.java
+++ DASPRO SEMESTER 1/jobsheet-11/Star4.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class Star4 {
     public static void main(String[] args) {
-        
+
         Scanner input04= new Scanner(System.in);
         System.out.print("masukkan nilai N =");
         int N = input04.nextInt();
 
-        for(int i=1;i<=N; i++){
+        for(int i=0;i<=N; i++){
             System.out.print("*");
        }
     }
