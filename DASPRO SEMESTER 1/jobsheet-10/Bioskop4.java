9773bf3d5aff9d86a44b9ec84c316a3e46ae8304
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-11-08 09:11:04 +0700

    modifikasi menampilkan nama penonton for loop

--- DASPRO SEMESTER 1/jobsheet-10/Bioskop4.java
+++ DASPRO SEMESTER 1/jobsheet-10/Bioskop4.java
@@ -1,7 +1,6 @@
 public class Bioskop4{
     public static void main(String[] args) {
         String [][] penonton = new String [4][2];
-        
         penonton [0][0]= "Amin";
         penonton [0][1]= "Bena";
         penonton [1][0]= "Candra";
@@ -20,5 +19,9 @@ public class Bioskop4{
        for (String[] barisPenonton : penonton){
         System.out.println("Panjang baris: " +barisPenonton.length);
        }
+        System.out.println("Penonton pada baris ke-3: ");
+        for (int i=0;i<penonton[2].length; i++){
+            System.out.println(penonton[2][i]);
+        }
     }
 }
