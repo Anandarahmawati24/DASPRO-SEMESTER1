c7dc713ea4175d00f6ae48f461ddff6109a629aa
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-11-22 14:29:11 +0700

    kode program percobaan 4

--- DASPRO SEMESTER 1/jobsheet-13/UcapanTerimakasih_4.java
+++ DASPRO SEMESTER 1/jobsheet-13/UcapanTerimakasih_4.java
@@ -10,7 +10,7 @@ public class UcapanTerimakasih_4 {
     }
     public static void UcapanTerimaKasih(){
         String nama = PenerimaUcapan();
-        System.out.println("Thank you "+nama+"for being the best teacher in the world.\n"+
+        System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
     public static void main(String[] args) {
