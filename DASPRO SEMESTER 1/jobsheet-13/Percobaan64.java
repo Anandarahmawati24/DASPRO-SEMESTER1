bd08967727925bb37a80186bdca2b2e3e7ff66d0
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-11-23 18:41:35 +0700

    percobaan 6 dengan fungsi

--- DASPRO SEMESTER 1/jobsheet-13/Percobaan64.java
+++ DASPRO SEMESTER 1/jobsheet-13/Percobaan64.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 public class Percobaan64 {
+        Scanner input = new Scanner(System.in);
+    static int hitungLuas (int pjg, int lb){
+        int Luas= pjg*lb;
+        return Luas;
+    }
+    static int hitungVolume (int tinggi, int a, int b){
+        int volume = hitungLuas(a,b)*tinggi;
+        return volume;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-       
         int p,l,t,L,vol;
-
-    System.out.println("Masukkan panjang");
-    p=input.nextInt();
-    System.out.println("Masukkan lebar");
-    l=input.nextInt();
-    System.out.println("Masukkan tinggi");
-    t= input.nextInt();
-    
-    L=p*l;
-    System.out.println("Luas persegi panjang adalah "+L);
-
-    vol=p*l*t;
-    System.out.println("Volume balok adalah "+vol);
+        System.out.println("Masukkan panjang");
+        p= input.nextInt();
+        System.out.println("Masukkan lebar");
+        l=input.nextInt();
+        System.out.println("Masukkan tinggi");
+        t=input.nextInt();
+        L=hitungLuas(p,l);
+        System.out.println("Luas persegi panjang adalah "+L);
+        vol= hitungVolume(t, p,l );
+        System.out.println(" Volume Balok adalah "+vol);
     }
 }
