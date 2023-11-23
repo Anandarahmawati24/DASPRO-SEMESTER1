1d2269afc2118cdc79549e0a55121e00c281f5d1
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-11-23 18:27:32 +0700

    percobaa 6 tanpa fungsi

--- DASPRO SEMESTER 1/jobsheet-13/Percobaan64.java
+++ DASPRO SEMESTER 1/jobsheet-13/Percobaan64.java
@@ -18,28 +18,4 @@ public class Percobaan64 {
     vol=p*l*t;
     System.out.println("Volume balok adalah "+vol);
     }
-    
-    static int hitungLuas (int pjg, int lb){
-        int Luas= pjg*lb;
-        return Luas;
-    }
-    static int hitungVolume (int tinggi, int a, int b){
-        int volume = hitungLuas(a,b)*tinggi;
-        return volume;
-    }
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int p,l,t,L,vol;
-        System.out.println("Masukkan panjang");
-        p= input.nextInt();
-        System.out.println("Masukkan lebar");
-        l=input.nextInt();
-        System.out.println("Masukkan tinggi");
-        t=input.nextInt();
-
-        L=hitungLuas(p,L);
-        System.out.println("Luas persegi panjang adalah "+L);
-        vol= hitungVolume(t, L, vol);
-        System.out.println(" Volume Balok adalah "+vol);
-    }
 }
