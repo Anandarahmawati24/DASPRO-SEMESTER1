42f417893ba7bad3bd88431d501bcab1dfc9171d
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-11-29 14:21:20 +0700

    modifikasi program

--- DASPRO SEMESTER 1/jobsheet-14/Percobaan2.java
+++ DASPRO SEMESTER 1/jobsheet-14/Percobaan2.java
@@ -4,17 +4,24 @@ public class Percobaan2 {
         if (y ==0 ){
             return (1);
         } else{
-            return (x * hitungPangkat (x, y-1 ));
+            System.out.print(x + "x");
+            int hasilPangkat = x*hitungPangkat(x, y -1);
+            if (y==1){
+                System.out.print("1");
+            }
+            return hasilPangkat;
         }
     }
 public static void main(String[] args) {
    Scanner sc = new Scanner (System.in);
    int bilangan , pangkat;
- System.out.println("Bilangan yang dihitung: ");
+ System.out.print("Bilangan yang dihitung: ");
  bilangan = sc.nextInt();
- System.out.println(" Pangkat: ");
+ System.out.print("Pangkat: ");
  pangkat = sc.nextInt();
 
- System.out.println(hitungPangkat(bilangan, pangkat));
+ System.out.print("Hasil perhitungan: ");
+ int hasil = hitungPangkat(bilangan, pangkat);
+ System.out.print(" = " +hasil);
 }
 }
